c0081e464212ae4862610bb541116b59f4a0423d
Sean Redmond <github-smr@sneakemail.com>
2015-06-18 13:09:37 -0400

    Add Footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,34 @@
+(function () {
+    "use strict";
+    var React = require('react'),
+        Footer = React.createClass({
+            render: function () {
+                return (
+                        <div id="footer">
+
+                        <ul id="footerColumn1">
+                        <li>About</li>
+                        <li>Press</li>
+                        <li>Careers</li>
+                        <li>Space Rental</li>
+                        </ul>
+
+                        <ul id="footerColumn2">
+                        <li>Shop</li>
+                        <li>Governance</li>
+                        <li>Press</li>
+                        </ul>
+
+                        <ul id="footerColumn3">
+                        <li>Terms &amp; Conditions</li>
+                        <li>Accessibility</li>
+                        <li>Language</li>
+                        </ul>
+                        </div>
+
+                        <div>© The New York Public Library, 2015</div>
+                );
+            }
+        });
+    module.exports = Footer;
+}());
