f82f7e4c4c633285d8c550057bf3d6303809c302
Sean Redmond <github-smr@sneakemail.com>
2015-06-29 16:50:24 -0400

    Use better ES6 idiom, style with Radium

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,43 +1,58 @@
 import React from 'react';
 import Radium from 'radium';
 
-let Footer = React.createClass({
+class Footer extends React.Component {
     render () {
-	let styles = {
-	    base: {
-		background: 'red',
-	    }
-	};
 	
         return (
                 <div id="footer" style={styles.base}>
 		
-                <div id="footerLinks">
-                <ul id="footerColumn1">
+                <div id="footerLinks" style={styles.footerLinks}>
+                <ul id="footerColumn1" style={styles.columns}>
                 <li>About</li>
                 <li>Press</li>
                 <li>Careers</li>
                 <li>Space Rental</li>
                 </ul>
 		
-                <ul id="footerColumn2">
+                <ul id="footerColumn2" style={styles.columns}>
                 <li>Shop</li>
                 <li>Governance</li>
                 <li>Press</li>
                 </ul>
 		
-                <ul id="footerColumn3">
+                <ul id="footerColumn3" style={styles.columns}>
                 <li>Terms &amp; Conditions</li>
                 <li>Accessibility</li>
                 <li>Language</li>
                 </ul>
                 </div>
 		
-                <div>© The New York Public Library, 2015</div>
-		</div>
-        );
+                <div style={styles.dim}>© The New York Public Library, 2015</div>
+	    </div>
+	);
     }
-});
+}
 
-module.exports = Radium(Footer);
+const styles = {
+    base: {
+	background: '#0F729F',
+	color: 'white',
+	paddingLeft: '236px'
+    },
+    footerLinks: {
+	display: 'flex',
+	lineHeight: 2,
+	marginBottom: "8.75em",
+	paddingTop: "1em"
+    },
+    columns: {
+	width: '10em'
+    },
+    dim: {
+	color: '#699CBA'
+    }
+};
+
+export default Radium(Footer);
 
