22a0fa2fe874ad6ea0c07ae5cbb1f95d632918a3
Edwin Guzman <edwin.gzmn@gmail.com>
2015-12-01 22:49:31 -0500

    Adding dist folder.

---
+++ dist/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _FooterJs = require('./Footer.js');
+
+var _FooterJs2 = _interopRequireDefault(_FooterJs);
+
+require('./styles/styles.scss');
+
+/* app.jsx
+ * Used for local development of React Components
+ */
+_react2['default'].render(_react2['default'].createElement(_FooterJs2['default']), document.body);
