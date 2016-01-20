f737ca7d4cd07ed43b384eff7a906c6c608928d6
Edwin Guzman <edwin.gzmn@gmail.com>
2016-01-20 10:29:10 -0500

    Adding changes, but still working on using just flexbox.

--- src/Footer.js
+++ src/Footer.js
@@ -16,61 +16,63 @@ class Footer extends React.Component {
   render() {
     return (
       <footer id={this.props.id} className={this.props.className}>
-        <div id={`${this.props.id}-content`}
-          className={`${this.props.className}-content`}>
+        <div>
+          <div id={`${this.props.id}-content`}
+            className={`${this.props.className}-content`}>
 
-          <ul className="footerLinks">
-            <li>
-              <a href="/help/about-nypl">About NYPL</a>
-              <ul>
-                <li><a href="/help/about-nypl/media-center">Press</a></li>
-                <li><a href="/careers">Careers</a></li>
-                <li><a href="/spacerental">Space Rental</a></li>
-              </ul>
-            </li>
-
-            <li>
-              <a href="/help/about-nypl/legal-notices/privacy-policy">
-                Privacy Policy
-              </a>
-              <ul>
-              <li><a href="/policies">Other Policies</a></li>
-              <li><a href="/terms-conditions">Terms & Conditions</a></li>
+            <ul className="footerLinks">
               <li>
-                <a href="/help/about-nypl/leadership/board-trustees">
-                  Governance
-                </a>
+                <a href="/help/about-nypl">About NYPL</a>
+                <ul>
+                  <li><a href="/help/about-nypl/media-center">Press</a></li>
+                  <li><a href="/careers">Careers</a></li>
+                  <li><a href="/spacerental">Space Rental</a></li>
+                </ul>
               </li>
-              </ul>
-            </li>
 
-            <li>
-              <a href="/help/about-nypl/legal-notices/rules-and-regulations">
-                Rules & Regulations
-              </a>
-              <ul>
               <li>
-                <a href="/help/community-outreach/services-for-persons-with-disabilities">
-                  Accessibility
+                <a href="/help/about-nypl/legal-notices/privacy-policy">
+                  Privacy Policy
                 </a>
+                <ul>
+                <li><a href="/policies">Other Policies</a></li>
+                <li><a href="/terms-conditions">Terms & Conditions</a></li>
+                <li>
+                  <a href="/help/about-nypl/leadership/board-trustees">
+                    Governance
+                  </a>
+                </li>
+                </ul>
               </li>
-              <li><a href="/language">Language</a></li>
-              </ul>
-            </li>
 
-          </ul>
+              <li>
+                <a href="/help/about-nypl/legal-notices/rules-and-regulations">
+                  Rules & Regulations
+                </a>
+                <ul>
+                <li>
+                  <a href="/help/community-outreach/services-for-persons-with-disabilities">
+                    Accessibility
+                  </a>
+                </li>
+                <li><a href="/language">Language</a></li>
+                </ul>
+              </li>
 
-          <SocialMediaList
-            data={data.SocialMedia}
-            id="SocialMediaList"
-            className="socialmedia" />
+            </ul>
+
+            <SocialMediaList
+              data={data.SocialMedia}
+              id="SocialMediaList"
+              className="socialmedia" />
+          </div>
+
+          <div id="copyright" className="copyright">
+            <p>© The New York Public Library, {new Date().getFullYear()}</p>
+          </div>
+
+          <span className="logoText nypl-icon-logo-type"></span>
         </div>
-
-        <div id="copyright" className="copyright">
-          <p>© The New York Public Library, {new Date().getFullYear()}</p>
-        </div>
-
-        <span className="logoText nypl-icon-logo-type"></span>
       </footer>
     );
   }
