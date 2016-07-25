45c0018312036d8976b2fb3e7236ecba493b0f10
Kang Peng <kangtingpeng@nypl.org>
2016-07-25 12:37:24 -0400

    Imported full reactDOM and pass it to a11y.

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import Footer from './Footer.jsx';
  
 import './styles/styles.scss';
 
 import a11y from 'react-a11y';
 
-a11y(React, { render, includeSrcNode: true });
+a11y(React, { ReactDOM: ReactDOM , includeSrcNode: true });
 
 /* app.jsx
  * Used for local development of React Components
  */
-render(<Footer id="footer" className="footer" />, document.getElementById('footer'));
+ReactDOM.render(<Footer id="footer" className="footer" />, document.getElementById('footer'));
