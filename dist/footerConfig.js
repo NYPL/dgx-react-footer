d36d0bd16eca043f575430a7b67b1c004e4aa808
Kang Peng <kangtingpeng@nypl.org>
2016-07-28 18:13:27 -0400

    Updated link label of social media on Footer.

--- dist/footerConfig.js
+++ dist/footerConfig.js
@@ -27,19 +27,19 @@ var data = {
   }, { name: 'Language',
     link: '/language'
   }]],
-  socialMedia: [{ name: 'Facebook',
+  socialMedia: [{ name: 'NYPL on Facebook',
     link: 'https://www.facebook.com/nypl',
     className: 'nypl-icon-facebook'
-  }, { name: 'Twitter',
+  }, { name: 'NYPL on Twitter',
     link: 'https://twitter.com/nypl',
     className: 'nypl-icon-twitter'
-  }, { name: 'Instagram',
+  }, { name: 'NYPL on Instagram',
     link: 'https://instagram.com/nypl',
     className: 'nypl-icon-instagram'
-  }, { name: 'Tumblr',
+  }, { name: 'NYPL on Tumblr',
     link: 'https://nypl.tumblr.com/',
     className: 'nypl-icon-tumblr'
-  }, { name: 'Youtube',
+  }, { name: 'NYPL on Youtube',
     link: 'https://www.youtube.com/user/NewYorkPublicLibrary',
     className: 'nypl-icon-youtube'
   }]
